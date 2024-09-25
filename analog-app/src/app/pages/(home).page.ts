9021d221d5da4c91fb2ff5685903614234d65ae1
Will Stewart <william.stewart@finbourne.com>
2024-09-25 15:35:25 +0100

    Initial commit

---
+++ analog-app/src/app/pages/(home).page.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+import { AnalogWelcomeComponent } from './analog-welcome.component';
+
+@Component({
+  selector: 'analog-app-home',
+  standalone: true,
+  imports: [AnalogWelcomeComponent],
+  template: `
+     <analog-app-analog-welcome/>
+  `,
+})
+export default class HomeComponent {
+}
